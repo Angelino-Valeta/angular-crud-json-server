1f4a8d6c1e5494631c6803eb86ddc5f2bc1eefa1
Angelino Valeta <angelinovaleta@gmail.com>
2021-08-28 21:23:31 +0100

    first commit

---
+++ src/app/movies/movies-list/movies-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movies-list',
+  templateUrl: './movies-list.component.html',
+  styleUrls: ['./movies-list.component.css']
+})
+export class MoviesListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.movies = [
+        {
+          title: "Spider-Man",
+          releaseDate: new Date(),
+          price: 1400.99
+        },
+        {
+          title: "Moana",
+          releaseDate: new Date("2021-11-14"),
+          price: 300.99
+        }
+      ]
+    },2000)
+  }
+
+  movies;
+
+}
